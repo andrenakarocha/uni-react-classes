3f954eee823ef59c842882cbd54da06fb250d06a
André Rocha <andrenakarocha@hotmail.com>
2024-10-15 08:50:10 -0300

    CardRecomenda component

---
+++ aulas/site-book-store/src/components/CardRecomenda/CardRecomenda.jsx
@@ -0,0 +1,20 @@
+import { Titulo } from '../Titulo/Titulo.styles'
+import { Card, Subtitulo, ImgLivro, Botao, Descricao } from './CardRecomenda.styles'
+
+function CardRecomenda({titulo, subtitulo, descricao, img}) { 
+    return (
+        <Card>
+            <div>
+                <Titulo tamanhoFonte='16px' cor='#EB9B00' alinhamento='left'>{titulo}</Titulo>
+                <Subtitulo>{subtitulo}</Subtitulo>
+                <Descricao>{descricao}</Descricao>
+            </div>
+            <div>
+                <ImgLivro src={img}/>
+                <Botao>Saiba mais</Botao>
+            </div>
+        </Card>
+    )
+}
+
+export default CardRecomenda
